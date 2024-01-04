7b696e119f7a0789bb6898d6d1ef607828f4ae4a
bryantt1 <bryantyanhui@gmail.com>
2024-01-05 00:34:57 +0800

    redo map creation

--- game.py
+++ game.py
@@ -1,25 +1,72 @@
-def empty_map():
-    map = []
-    for r in range(20):
-        temp = []
-        for c in range(20):
-            temp.append("")
-        map.append(temp)
-    return map
+#SDD Assignment 1
+
+import random
+
+map = [[[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]],
+         [[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None],[None,None]]
+    ]
+
+rowLetters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T']
 
 def show_map():
-    map_str = ""
+    borders = "+-----"
+    end = "+"
+    rows = len(map)
+    columns = len(map[0])
+    for i in range(0, 19):
+        print("{:>6}".format(i+1), end="")
+    print("{:>6}".format("20"))
+    print()
+    
+    print("  ",end="")
+    for i in range(columns):
+        print(borders,end="")
+    print(end)
+    
+    for i in range(rows):
+        print(rowLetters[i],end="")
+        print(" ",end="")
 
-    for i in range(20):
-        # Draw the top border of the box
-        map_str += "+----" * 20 + "+\n"
+        for j in map[i]:
+            print("|",end="")
+            if j[0] == None:
+                print("{:^5}".format(""),end="")
+            else:
+                print("{:^5}".format(str(j[0])),end="")
+        print("|")
+        print("  ",end="")
 
-        # Draw the vertical borders of the box
-        map_str += "|    " * 20 + "|\n"
+        for j in map[i]:
+            print("|",end="")
+            if j[1] == None:
+                print("{:^5}".format(""),end="")
+            else:
+                print("{:2}/{:2}".format(j[1],j[2]),end="")
+        print("|")
 
-    # Draw the bottom border of the last row
-    map_str += "+----" * 20 + "+"
-    print(map_str)
+        print("  ",end="")  #To leave a space in the column that has a letter
+        
+        for i in range(columns):
+            print(borders,end="")
+        print(end)
 
 def print_menu():
     print("1. Build a building")
@@ -28,44 +75,38 @@ def print_menu():
     print("3. Save game")
     print("4. Exit to main menu")
 
-def turn_counter(map):
-    temp2=0
-    for r in map:
-        temp = r.count("")
-        temp2+=temp
-    return(20*20-temp2)
-
-while True:
-    print("Welcome to Ngee Ann City!")
-    print("{:-^28}".format(""))
-    print("1. Start new game\n2.Load previous save\n3. Show high scores\n4. Exit game")
-    
-    try:
-        option = int(input("Please enter your choice: "))
-    except:
+def game_start():
+    while True:
+        print("Welcome to Ngee Ann City!")
+        print("{:-^28}".format(""))
+        print("1. Start new game\n2.Load previous save\n3. Show high scores\n4. Exit game")
+        
+        try:
+            option = int(input("Please enter your choice: "))
+        except:
+            print()
+            print("Error.. invalid input!! Try again!")
+            continue
         print()
-        print("Error.. invalid input!! Try again!")
-        continue
-    print()
-    if option not in range(5):
-        print("Error.. invalid input!! Try again!")
-        continue
+        if option not in range(5):
+            print("Error.. invalid input!! Try again!")
+            continue
 
-    if option == 1:
-        map = empty_map()
+        if option == 1:
+            show_map()
+            start_turn()
 
-    elif option == 2:
-        a = 1
-    
-    elif option == 3:
-        b = 1
+        elif option == 2:
+            a = 1
+        
+        elif option == 3:
+            b = 1
 
-    elif option == 4:
-        print("Thanks for playing!")
-        break
+        elif option == 4:
+            print("Thanks for playing!")
+            break
 
-    turn = turn_counter(map)
-
+def start_turn():
     while turn<400:
         turn+=1
 
